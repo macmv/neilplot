87017499b0009ff86a8a7124d111d376f69ea4a5
Neil Macneale V <neil.macneale.v@gmail.com>
2025-11-25 17:36:37 -0800

    Add a file for bounds

---
+++ src/bounds.rs
@@ -0,0 +1,129 @@
+use vello::kurbo::Affine;
+
+#[derive(Clone, Copy)]
+pub struct Bounds {
+  pub x: Range,
+  pub y: Range,
+}
+
+#[derive(Clone, Copy)]
+pub struct Range {
+  pub min: f64,
+  pub max: f64,
+}
+
+impl Bounds {
+  pub const fn empty() -> Self { Bounds { x: Range::empty(), y: Range::empty() } }
+  pub const fn new(x: Range, y: Range) -> Self { Bounds { x, y } }
+
+  pub const fn shrink(self, amount: f64) -> Self {
+    Bounds { x: self.x.shrink(amount), y: self.y.shrink(amount) }
+  }
+  pub const fn shrink_by(self, fract: f64) -> Self {
+    Bounds { x: self.x.shrink_by(fract), y: self.y.shrink(fract) }
+  }
+
+  pub const fn expand(self, amount: f64) -> Self {
+    Bounds { x: self.x.expand(amount), y: self.y.expand(amount) }
+  }
+  pub const fn expand_by(self, fract: f64) -> Self {
+    Bounds { x: self.x.expand_by(fract), y: self.y.expand_by(fract) }
+  }
+
+  pub fn union(&self, other: Bounds) -> Bounds {
+    Bounds { x: self.x.union(other.x), y: self.y.union(other.y) }
+  }
+
+  pub(crate) fn transform_to(&self, viewport: Bounds) -> Affine {
+    let scale_x = viewport.x.size() / self.x.size();
+    let scale_y = viewport.y.size() / self.y.size();
+    let translate_x = viewport.x.min - self.x.min * scale_x;
+    let translate_y = viewport.y.min - self.y.min * scale_y;
+
+    Affine::new([scale_x, 0.0, 0.0, scale_y, translate_x, translate_y])
+  }
+}
+
+impl Default for Range {
+  fn default() -> Self { Range::empty() }
+}
+
+impl Range {
+  pub const fn empty() -> Self { Range { min: 0.0, max: 0.0 } }
+  pub const fn new(min: f64, max: f64) -> Self { Range { min, max } }
+  pub const fn size(&self) -> f64 { self.max - self.min }
+
+  pub const fn shrink(self, amount: f64) -> Self { self.expand(-amount) }
+  pub const fn shrink_by(self, fract: f64) -> Self { self.shrink(self.size() * fract) }
+  pub const fn expand(self, amount: f64) -> Self {
+    Range {
+      min: self.min - amount * self.size().signum(),
+      max: self.max + amount * self.size().signum(),
+    }
+  }
+  pub const fn expand_by(self, fract: f64) -> Self { self.expand(self.size() * fract) }
+
+  pub const fn contains(&self, value: &f64) -> bool {
+    (*value >= self.min && *value <= self.max) || (*value <= self.min && *value >= self.max)
+  }
+
+  pub fn union(&self, other: Range) -> Range {
+    if self.size() == 0.0 {
+      other
+    } else if other.size() == 0.0 {
+      *self
+    } else {
+      Range { min: self.min.min(other.min), max: self.max.max(other.max) }
+    }
+  }
+
+  pub fn nice_ticks(&self, count: u32) -> TicksIter {
+    let step = (self.max - self.min) / f64::from(count);
+    let k = step.log10().floor();
+    let base = step / 10f64.powf(k);
+
+    let nice_base = match base {
+      b if b < 1.0 => 1.0,
+      b if b < 2.0 => 2.0,
+      b if b < 2.5 => 2.5,
+      b if b < 5.0 => 5.0,
+      _ => 10.0,
+    };
+
+    let step = nice_base * 10f64.powf(k);
+    let lo = (self.min / step).floor() * step;
+    let hi = (self.max / step).ceil() * step;
+
+    let precision = (-k as i32 + 4).max(0) as usize;
+    TicksIter::new(lo, hi, step, precision)
+  }
+}
+
+pub struct TicksIter {
+  current:   f64,
+  step:      f64,
+  hi:        f64,
+  precision: usize,
+}
+
+impl TicksIter {
+  fn new(lo: f64, hi: f64, step: f64, precision: usize) -> Self {
+    TicksIter { current: lo, step, hi, precision }
+  }
+
+  pub fn precision(&self) -> usize { self.precision }
+}
+
+impl Iterator for TicksIter {
+  type Item = f64;
+  fn next(&mut self) -> Option<Self::Item> {
+    if self.current < self.hi + self.step * 0.5 {
+      let p = 10f64.powi(self.precision as i32);
+      let result = (self.current * p).round() / p;
+      self.current += self.step;
+      Some(result)
+    } else {
+      None
+    }
+  }
+}
