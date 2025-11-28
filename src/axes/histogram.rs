caff22123b182d7e96226a27353ca146c092647a
Neil Macneale V <neil.macneale.v@gmail.com>
2025-11-28 11:24:38 -0800

    Draw histograms

--- src/axes/histogram.rs
+++ src/axes/histogram.rs
@@ -1,3 +1,4 @@
+use kurbo::{Affine, BezPath, Point};
 use polars::prelude::*;
 
 use crate::render::Render;
@@ -19,5 +20,20 @@ impl<'a> HistogramAxes<'a> {
     )
   }
 
-  pub(crate) fn draw(&self, render: &mut Render, transform: vello::kurbo::Affine) {}
+  pub(crate) fn draw(&self, render: &mut Render, transform: Affine) {
+    let mut path = BezPath::new();
+    path.move_to(Point::new(0.0, 0.0));
+
+    for x in 0..self.counts.len() {
+      let count = self.counts.get(x).unwrap().try_extract::<f64>().unwrap();
+
+      path.line_to(Point::new(x as f64, count));
+      path.line_to(Point::new(x as f64 + 1.0, count));
+    }
+
+    path.line_to(Point::new(self.counts.len() as f64, 0.0));
+    path.close_path();
+
+    render.fill(&path, transform, crate::theme::ROCKET.sample(0.0));
+  }
 }
