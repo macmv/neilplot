e83e0664e17466660bb6d548091328b4aeecf0f5
Neil Macneale V <neil.macneale.v@gmail.com>
2025-11-28 11:50:50 -0800

    Make a random array and count it

--- examples/histogram.rs
+++ examples/histogram.rs
@@ -2,15 +2,21 @@ use neilplot::Plot;
 use polars::prelude::*;
 
 fn main() -> PolarsResult<()> {
-  let df = df! {
-    "label" => &["A", "B", "C", "D", "E"],
-    "counts" => &[5, 10, 3, 8, 2],
-  }?;
+  let column = ChunkedArray::<Float64Type>::rand_standard_normal("rand".into(), 1000);
+  let df = DataFrame::new(vec![column.into_series().into()])?;
 
   let mut plot = Plot::new();
   plot.title("Foo");
   plot.x.title("Label");
   plot.y.title("Counts");
+
+  let df = df
+    .lazy()
+    .with_column((col("rand") * lit(10)).floor().cast(DataType::Int32).alias("rand"))
+    .group_by([col("rand")])
+    .agg([col("rand").count().alias("counts")])
+    .sort(["rand"], SortMultipleOptions::new())
+    .collect()?;
 
   plot.histogram(df.column("counts")?);
 
